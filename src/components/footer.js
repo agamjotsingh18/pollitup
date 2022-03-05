5485a79a28ad00b4adb66099e18905bd6873c490
Shubham29012001 <smmaurya29012001@gmail.com>
2022-03-05 22:56:40 +0530

    Add Copyright Year to Be Dynamic

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import {
   Stack,
   Text,
   Link as ChakraLink,
- 
+
 } from '@chakra-ui/react';
 
 
@@ -16,21 +16,21 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function FooterComponent() {
   return (
-    <Flex p={4}  as="footer"
+    <Flex p={4} as="footer"
       justify="space-between" direction={{ base: "column", md: "row" }}
       bg="brand.800" color="white"
     >
       <Flex align="center" direction={{ base: "column", md: "row" }}>
-        <Text m={2} color="gray.400">&copy; 2021 PollItUp</Text>
+        <Text m={2} color="gray.400">&copy; {new Date().getFullYear()} PollItUp - All Rights Reserved</Text>
         <Stack m={2} spacing={3} direction="row" align="center">
           {/*<RLink to="/contact">Contact us</RLink>
           <RLink to="/reportissue">Report an issue</RLink>*/}
         </Stack>
       </Flex>
       <Stack m={2} spacing={4} direction="row" justify="center" align="center">
-        
+
         <ChakraLink href="https://github.com/agamjotsingh18/pollitup.github.io" target="_blank">
-          <FontAwesomeIcon icon={faGithub}/>
+          <FontAwesomeIcon icon={faGithub} />
         </ChakraLink>
       </Stack>
     </Flex>
