fe33fa8b1d3d54c4bd6d88fb9ff58710a64d60c8
Ayush <ayushthth@gmail.com>
2022-03-24 23:11:50 +0530

    JWT-AuthAdded

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt=require('jsonwebtoken')
+const User=require('../models/userModel')
+const auth=async (req,res,next)=>{
+    try{
+        const token=req.header('auth-token')
+        const decoded=jwt.verify(token,"hello")
+        const user= await User.findOne({_id:decoded._id,'tokens.token':token})
+        if(!user){
+           throw new Error()
+       }
+       req.token=token   
+       req.user=user
+       next()    
+    }catch(e){
+        res.status(401).send({'error':'Please Authenticate'})
+    }
+}
+module.exports=auth
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,27 +2,54 @@ const mongoose=require('mongoose')
 const express=require('express')
 const User=require('../models/userModel')
 const router=new express.Router()
+const auth=require('../middleware/auth')
 
 //create user(register)
 router.post('/registerUser',async (req,res)=>{
     const user=new User(req.body)
     try{
         await user.save()
-        res.status(201).send(user)
+        const token=await user.generateAuthToken()  
+        res.status(201).send({user,token})
     }catch(e){
         res.status(400).send("email is incorrect or already registered")
     }
 })
 
-
 //user login
 router.post('/loginUser',async (req,res)=>{
     try{
         const user = await User.findByCredentials(req.body.email,req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send(token)
     }catch(e){
         res.status(400).send('Login Failed')
     }
 })
+
+//user logout
+router.post('/logout',auth,async (req,res)=>{
+    try{
+        req.user.tokens=req.user.tokens.filter((token)=>{
+            return token.token!==req.token
+        })                         
+        await req.user.save()      
+        res.send()                 
+    }catch(e){
+        res.status(500).send()    
+    }
+})  
+
+//user logout from all devices
+router.post('/logoutAll',auth,async (req,res)=>{
+    try{
+          req.user.tokens=[]         
+          await req.user.save()      
+          res.send()
+    }catch(e){
+          res.status(500).send()
+    }
+})
+
 
 module.exports=router
