95f9d75ab5e29d34c6309496692f44cb4768438c
An525ish <an525ish@gmail.com>
2023-05-23 13:15:39 +0530

    update git ignore

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link as RLink } from "react-router-dom";
-import ColorModeToggle from "./colorModeToggle";
-import { Slide } from "@chakra-ui/react";
+import React from 'react';
+import { Link as RLink } from 'react-router-dom';
+import ColorModeToggle from './colorModeToggle';
+import { Slide } from '@chakra-ui/react';
 
 import {
   Box,
@@ -12,11 +12,11 @@ import {
   Button,
   useColorModeValue,
   transition,
-} from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import { useAuth } from "../lib/auth";
-import { useState } from "react";
-import { useEffect } from "react";
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { useAuth } from '../lib/auth';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function NavbarComponent() {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -29,34 +29,34 @@ export default function NavbarComponent() {
     const handleScroll = () => {
       setSticky(window.scrollY > 120);
     };
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   });
 
   return (
     <Flex
-      as="nav"
+      as='nav'
       {...(sticky && {
         sx: {
-          pos: "sticky",
-          top: "0",
-          transition: "all .3s ease-in-out",
-          h: "90px",
-          zIndex: "overlay",
-          bg: "white",
-          boxShadow: "lg",
+          pos: 'sticky',
+          top: '0',
+          transition: 'all .3s ease-in-out',
+          h: '90px',
+          zIndex: 'overlay',
+          bg: 'white',
+          boxShadow: 'lg',
         },
       })}
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      w="100%"
+      align='center'
+      justify='space-between'
+      wrap='wrap'
+      w='100%'
       py={4}
       px={8}
-      bg={["brand.500", "brand.500", "transparent", "transparent"]}
-      color={["white", "white", "brand.500", "brand.500"]}
+      bg={['brand.500', 'brand.500', 'transparent', 'transparent']}
+      color={['white', 'white', 'brand.500', 'brand.500']}
     >
       {/* Logo */}
       <Logo />
@@ -66,29 +66,29 @@ export default function NavbarComponent() {
 
       {/* Links */}
       <Box
-        display={{ base: isOpen ? "block" : "none", md: "block" }}
-        flexBasis={{ base: "100%", md: "auto" }}
+        display={{ base: isOpen ? 'block' : 'none', md: 'block' }}
+        flexBasis={{ base: '100%', md: 'auto' }}
       >
         <Stack
           spacing={8}
-          align="center"
-          justify={["center", "space-between", "flex-end", "flex-end"]}
-          direction={["column", "row", "row", "row"]}
+          align='center'
+          justify={['center', 'space-between', 'flex-end', 'flex-end']}
+          direction={['column', 'row', 'row', 'row']}
           pt={[4, 4, 0, 0]}
         >
-          <MenuItem to="/">
-            {user && !loadingUser ? "Dashboard" : "Home"}
+          <MenuItem to='/'>
+            {user && !loadingUser ? 'Dashboard' : 'Home'}
           </MenuItem>
-          <MenuItem to="/discover">Discover</MenuItem>
-          {user && !loadingUser && <MenuItem to="/create">Create</MenuItem>}
-          {user && !loadingUser && <MenuItem to="/profile">Profile</MenuItem>}
+          <MenuItem to='/discover'>Discover</MenuItem>
+          {user && !loadingUser && <MenuItem to='/create'>Create</MenuItem>}
+          {user && !loadingUser && <MenuItem to='/profile'>Profile</MenuItem>}
           <MenuItem
-            to={user && !loadingUser ? "/logout" : "/login"}
-            type="button"
+            to={user && !loadingUser ? '/logout' : '/login'}
+            type='button'
           >
-            {user && !loadingUser ? "Logout" : "Login/Register"}
+            {user && !loadingUser ? 'Logout' : 'Login/Register'}
           </MenuItem>
-          <ColorModeToggle color={useColorModeValue("brand.700", "white")} />
+          <ColorModeToggle color={useColorModeValue('brand.700', 'white')} />
         </Stack>
       </Box>
     </Flex>
@@ -98,8 +98,8 @@ export default function NavbarComponent() {
 function Logo() {
   return (
     <Box>
-      <RLink to="/">
-        <Heading as="h1" size="xl">
+      <RLink to='/'>
+        <Heading as='h1' size='xl'>
           Poll It Up
         </Heading>
       </RLink>
@@ -110,7 +110,7 @@ function Logo() {
 function Toggle(props) {
   const { toggle, isOpen } = props;
   return (
-    <Box display={{ base: "block", md: "none" }} p={2} onClick={toggle}>
+    <Box display={{ base: 'block', md: 'none' }} p={2} onClick={toggle}>
       {isOpen ? <CloseIcon /> : <HamburgerIcon w={5} h={5} />}
     </Box>
   );
@@ -119,18 +119,18 @@ function Toggle(props) {
 function MenuItem(props) {
   const { to, children, type, ...rest } = props;
 
-  const bgDesktop = useColorModeValue("brand.50", "brand.900");
-  const bgHoverDesktop = useColorModeValue("brand.100", "brand.800");
+  const bgDesktop = useColorModeValue('brand.50', 'brand.900');
+  const bgHoverDesktop = useColorModeValue('brand.100', 'brand.800');
 
   // Button menu item
-  if (type === "button") {
+  if (type === 'button') {
     return (
       <RLink to={to}>
         <Button
-          size="sm"
-          bg={["brand.400", "brand.400", bgDesktop, bgDesktop]}
+          size='sm'
+          bg={['brand.400', 'brand.400', bgDesktop, bgDesktop]}
           _hover={{
-            bg: ["brand.300", "brand.300", bgHoverDesktop, bgHoverDesktop],
+            bg: ['brand.300', 'brand.300', bgHoverDesktop, bgHoverDesktop],
           }}
         >
           {children}
@@ -141,7 +141,7 @@ function MenuItem(props) {
 
   return (
     <RLink to={to}>
-      <Text display="block" {...rest}>
+      <Text display='block' {...rest}>
         {children}
       </Text>
     </RLink>
