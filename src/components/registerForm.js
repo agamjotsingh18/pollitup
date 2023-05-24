6c99522bdb0fe9d8c24aee20de057b9ab71afa95
Pushpendra Singh <pushpendrasingh33@bitbucket.org>
2023-05-24 12:44:45 +0530

    Login & register form error message position done

--- src/components/registerForm.js
+++ src/components/registerForm.js
@@ -1,20 +1,39 @@
-import { Button, FormControl, FormLabel, Input, Stack, } from '@chakra-ui/react'
-import * as React from 'react'
-import { PasswordField } from './passwordField'
+import { Button, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import * as React from "react";
+import { PasswordField } from "./passwordField";
 
 export const RegisterForm = (props) => {
   return (
-      <Stack spacing="6">
-        <FormControl id="email">
-          <FormLabel>Email address</FormLabel>
-          <Input value={props.email} onChange={e => props.setEmail(e.target.value)} name="email" type="email" autoComplete="off" required />
-        </FormControl>
-        <PasswordField password = {props.password} setPassword = {props.setPassword} />
-        <PasswordField label="Confirm Password" password = {props.password2} setPassword = {props.setPassword2} />
-        <Button type="submit" colorScheme="blue" size="lg" fontSize="md" onClick = {props.signUp}>
-          Register
-        </Button>
-      </Stack>
-
-  )
-}
+    <Stack spacing="6">
+      <FormControl id="email">
+        <FormLabel>Email address</FormLabel>
+        <Input
+          value={props.email}
+          onChange={(e) => props.setEmail(e.target.value)}
+          name="email"
+          type="email"
+          autoComplete="off"
+          required
+        />
+      </FormControl>
+      <PasswordField
+        password={props.password}
+        setPassword={props.setPassword}
+      />
+      <PasswordField
+        label="Confirm Password"
+        password={props.password2}
+        setPassword={props.setPassword2}
+      />
+      <Button
+        type="submit"
+        colorScheme="blue"
+        size="lg"
+        fontSize="md"
+        onClick={props.signUp}
+      >
+        Register
+      </Button>
+    </Stack>
+  );
+};
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import initFirebase from '../lib/firebase';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { useAuth } from '../lib/auth';
-import { addDoc, getDoc } from '../lib/db';
+import initFirebase from "../lib/firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useAuth } from "../lib/auth";
+import { addDoc, getDoc } from "../lib/db";
 
 import { LoginForm } from "../components/loginForm";
-import { DividerWithText } from '../components/dividerWithText';
-import { FaGoogle } from 'react-icons/fa';
+import { DividerWithText } from "../components/dividerWithText";
+import { FaGoogle } from "react-icons/fa";
 
 import {
   Container,
@@ -20,8 +20,8 @@ import {
   useColorModeValue as mode,
   VisuallyHidden,
   useToast,
-} from '@chakra-ui/react';
-import Link from '../components/link';
+} from "@chakra-ui/react";
+import Link from "../components/link";
 
 export default function LoginPage() {
   initFirebase();
@@ -31,94 +31,128 @@ export default function LoginPage() {
   const [password, setPassword] = React.useState("");
 
   React.useEffect(() => {
-    if (user && !loadingUser) window.location.href = '/';
+    if (user && !loadingUser) window.location.href = "/";
   }, [user, loadingUser]);
 
   async function signIn() {
-    await firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((u) => { // u.user.uid
-      window.location.href = '/';
-    })
-    .catch(function(err) {
-      toast({
-        title: "Error",
-        description: err.message,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((u) => {
+        // u.user.uid
+        window.location.href = "/";
+      })
+      .catch(function (err) {
+        toast({
+          title: "Error",
+          description: err.message,
+          status: "error",
+          position: "top-right",
+          duration: 5000,
+          isClosable: true,
+        });
       });
-    });
   }
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
-    await firebase.auth().signInWithRedirect(provider)
-    .then(async (u) => {
-      // If is new user, create doc in users col
-      const existingUser = await getDoc('users', u.user.uid);
-      if (!existingUser) {
-        await addDoc('users', {
-          displayName: "",
-          description: "",
-          logo: ""
-        }, u.user.uid);
-      }
-      window.location.href = '/';
-    })
-    .catch(function(err) {
-      toast({
-        title: "Error",
-        description: err.message,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
+    await firebase
+      .auth()
+      .signInWithRedirect(provider)
+      .then(async (u) => {
+        // If is new user, create doc in users col
+        const existingUser = await getDoc("users", u.user.uid);
+        if (!existingUser) {
+          await addDoc(
+            "users",
+            {
+              displayName: "",
+              description: "",
+              logo: "",
+            },
+            u.user.uid
+          );
+        }
+        window.location.href = "/";
+      })
+      .catch(function (err) {
+        toast({
+          title: "Error",
+          description: err.message,
+          status: "error",
+          position: "top-right",
+          duration: 5000,
+          isClosable: true,
+        });
       });
-    });;
   }
 
-  if (!loadingUser && !user) return (
-    <Container maxW="container.sm" p={8}>
-      <Box bg={mode('gray.50', 'inherit')} minH="100vh" py="12" px={{ sm: '6', lg: '8' }}>
-        <Box maxW={{ sm: 'md' }} mx={{ sm: 'auto' }} w={{ sm: 'full' }}>
-          <Heading mt="6" textAlign="center" size="xl" fontWeight="extrabold">
-            Log in to your account
-          </Heading>
-          <Text mt="4" align="center" maxW="md" fontWeight="medium">
-            <span>Don&apos;t have an account?</span>
-            <Box
-              as="a"
-              marginStart="1"
-              href="#"
-              color={mode('blue.600', 'blue.200')}
-              _hover={{ color: 'blue.600' }}
-              display={{ base: 'block', sm: 'revert' }}
-            >
-              <Link href="/register" color="brand.500">Register</Link>
-            </Box>
-          </Text>
-        </Box>
-        <Box maxW={{ sm: 'md' }} mx={{ sm: 'auto' }} mt="8" w={{ sm: 'full' }}>
+  if (!loadingUser && !user)
+    return (
+      <Container maxW="container.sm" p={8}>
+        <Box
+          bg={mode("gray.50", "inherit")}
+          minH="100vh"
+          py="12"
+          px={{ sm: "6", lg: "8" }}
+        >
+          <Box maxW={{ sm: "md" }} mx={{ sm: "auto" }} w={{ sm: "full" }}>
+            <Heading mt="6" textAlign="center" size="xl" fontWeight="extrabold">
+              Log in to your account
+            </Heading>
+            <Text mt="4" align="center" maxW="md" fontWeight="medium">
+              <span>Don&apos;t have an account?</span>
+              <Box
+                as="a"
+                marginStart="1"
+                href="#"
+                color={mode("blue.600", "blue.200")}
+                _hover={{ color: "blue.600" }}
+                display={{ base: "block", sm: "revert" }}
+              >
+                <Link href="/register" color="brand.500">
+                  Register
+                </Link>
+              </Box>
+            </Text>
+          </Box>
           <Box
-            bg={mode('white', 'gray.700')}
-            py="8"
-            px={{ base: '4', md: '10' }}
-            shadow="base"
-            rounded={{ sm: 'lg' }}
+            maxW={{ sm: "md" }}
+            mx={{ sm: "auto" }}
+            mt="8"
+            w={{ sm: "full" }}
           >
-            <LoginForm email = {email} setEmail = {setEmail} signIn = {signIn} password = {password} setPassword = {setPassword}/>
-            <DividerWithText mt="6">or continue with</DividerWithText>
-            <SimpleGrid mt="6" columns={1} spacing="3">
-              <Button onClick={signInWithGoogle} color="currentColor" variant="outline">
-                <VisuallyHidden>Sign in with Google</VisuallyHidden>
-                <FaGoogle/>
-              </Button>
-            </SimpleGrid>
+            <Box
+              bg={mode("white", "gray.700")}
+              py="8"
+              px={{ base: "4", md: "10" }}
+              shadow="base"
+              rounded={{ sm: "lg" }}
+            >
+              <LoginForm
+                email={email}
+                setEmail={setEmail}
+                signIn={signIn}
+                password={password}
+                setPassword={setPassword}
+              />
+              <DividerWithText mt="6">or continue with</DividerWithText>
+              <SimpleGrid mt="6" columns={1} spacing="3">
+                <Button
+                  onClick={signInWithGoogle}
+                  color="currentColor"
+                  variant="outline"
+                >
+                  <VisuallyHidden>Sign in with Google</VisuallyHidden>
+                  <FaGoogle />
+                </Button>
+              </SimpleGrid>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Container>
-  );
-  
+      </Container>
+    );
+
   // Prevents flashing page when logged in
-  return (<></>); // TODO: add loading page
+  return <></>; // TODO: add loading page
 }
