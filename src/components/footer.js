f0750787bc58114159887b462eca88b0a74e5f08
Krushna <codeofkrushna@gmail.com>
2023-05-26 09:25:19 +0530

    ISSUE:#114 COMBINED THE BOTTOM FOOTER and CONTACT FORM OF COPYRIGHT FOOTER

--- src/components/footer.js
+++ src/components/footer.js
@@ -16,23 +16,28 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function FooterComponent() {
   return (
-    <Flex p={4} as="footer"
-      justify="space-between" direction={{ base: "column", md: "row" }}
-      bg="brand.800" color="white"
-    >
-      <Flex align="center" direction={{ base: "column", md: "row" }}>
-        <Text m={2} color="gray.400">&copy; {new Date().getFullYear()} PollItUp - All Rights Reserved</Text>
-        <Stack m={2} spacing={3} direction="row" align="center">
-          {/*<RLink to="/contact">Contact us</RLink>
-          <RLink to="/reportissue">Report an issue</RLink>*/}
-        </Stack>
+    <Flex p={4} as="footer" bg="brand.800" color="white">
+      <Flex align="center" justify="center" direction="column">
+        <Text m={2} color="white" letterSpacing={1}>Made with ❤️ by Agamjot</Text>
+        <Stack m={2} spacing={3} direction="row" align="center"></Stack>
       </Flex>
-      <Stack m={2} spacing={4} direction="row" justify="center" align="center">
+      <Flex as="footer-2">
+        <Flex>  
+          <Text m={2} color="gray.400">&copy; {new Date().getFullYear()} PollItUp - All Rights Reserved</Text>
+          <Stack m={2} spacing={3} direction="row" align="center">
+              {/* <RLink to="/contact">Contact us</RLink>
+              <RLink to="/reportissue">Report an issue</RLink> */}
+          </Stack>  
+        </Flex>
+        <Flex as="footer-socioicon">
+          <Stack m={2} spacing={4} direction="row" justify="center" align="center">
 
-        <ChakraLink href="https://github.com/agamjotsingh18/pollitup.github.io" target="_blank">
-          <FontAwesomeIcon icon={faGithub} />
-        </ChakraLink>
-      </Stack>
+            <ChakraLink href="https://github.com/agamjotsingh18/pollitup.github.io" target="_blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </ChakraLink>
+          </Stack>
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
