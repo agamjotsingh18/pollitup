7fdebb5b3872659f3c5b059d100e9be66546e1cc
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-05-27 18:41:18 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -15,20 +15,14 @@ for contributor in contributors:
     avatar_url = contributor['avatar_url']
     contributions = contributor['contributions']
 
-    contributor_info.append(f'<div align="center" style="display: inline-block; margin: 10px;">\n'
-                           f'  <a href="{profile_url}">\n'
-                           f'    <img src="{avatar_url}" alt="{username}" style="width: 50px; border-radius: 1000px;">\n'
-                           f'  </a><br/>\n'
-                           f'  <a href="{profile_url}">{username}</a><br/>\n'
-                           f'  {contributions} contributions\n'
-                           f'</div>')
+    contributor_info.append(f'| <a href="{profile_url}"><img src="{avatar_url}" width="100px;" alt="{username}"/><br /><sub><b>{username}</b></sub></a><br />[📖]({profile_url})')
 
 # Read existing README content
 with open("README.md", "r") as readme_file:
     readme_content = readme_file.read()
 
 # Update README with contributor information
-updated_content = readme_content + "\n\n## Contributors\n\n" + "\n".join(contributor_info)
+updated_content = readme_content.replace("<!-- ALL-CONTRIBUTORS-LIST:START -->", "<!-- ALL-CONTRIBUTORS-LIST:START -->\n<!-- prettier-ignore-start -->\n<!-- markdownlint-disable -->\n<table>\n<tr>\n" + "\n".join(contributor_info) + "\n</tr>\n</table>\n<!-- markdownlint-restore -->\n<!-- prettier-ignore-end -->\n\n<!-- ALL-CONTRIBUTORS-LIST:END -->")
 
 # Write the updated README
 with open("README.md", "w") as readme_file:
