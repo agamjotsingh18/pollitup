24cfee89c28386242bb616998032f012404cbb19
Arnabdaz <96580571+Arnabdaz@users.noreply.github.com>
2023-05-27 18:34:04 +0530

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -15,14 +15,20 @@ for contributor in contributors:
     avatar_url = contributor['avatar_url']
     contributions = contributor['contributions']
 
-    contributor_info.append(f'<div align="center"><img src="{avatar_url}" width="100px" alt="{username}"/><br/><a href="{profile_url}">{username}</a><br/>{contributions} contributions</div>')
+    contributor_info.append(f'<div align="center" style="display: inline-block; margin: 10px;">\n'
+                           f'  <a href="{profile_url}">\n'
+                           f'    <img src="{avatar_url}" alt="{username}" style="width: 50%; border-radius: 50%;">\n'
+                           f'  </a><br/>\n'
+                           f'  <a href="{profile_url}">{username}</a><br/>\n'
+                           f'  {contributions} contributions\n'
+                           f'</div>')
 
 # Read existing README content
 with open("README.md", "r") as readme_file:
     readme_content = readme_file.read()
 
 # Update README with contributor information
-updated_content = readme_content + "\n\n## Contributors\n\n<div align='center'>\n" + "\n".join(contributor_info) + "\n</div>"
+updated_content = readme_content + "\n\n## Contributors\n\n" + '<div align="center">\n' + "\n".join(contributor_info) + '\n</div>'
 
 # Write the updated README
 with open("README.md", "w") as readme_file:
