f0d8ad3fa862a771eb04fe7086e40b9bdedf651f
Harshit <harshitjoshi3238@gmail.com>
2023-06-11 12:33:36 +0530

    updated mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/curiomind',{})
-
 const connectToDatabase=()=>{console.log("Connected")}
 
-module.exports=connectToDatabase
+module.exports=connectToDatabase;
