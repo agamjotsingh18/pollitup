7b830867510b1d74f37115bdb0334880fa8cf6d7
Soumyajit <mondalsoumya02@gmail.com>
2023-06-28 22:04:24 +0530

    Updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,38 +1,103 @@
-import React from 'react';
-//import { Link as RLink } from 'react-router-dom';
-
 import {
-  Flex,
+  Box,
   Stack,
+  HStack,
+  VStack,
+  Link,
+  Divider,
+  Image,
   Text,
-  Link as ChakraLink,
-
+  Button,
+  useColorModeValue,
 } from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
+import { FiTwitter } from 'react-icons/fi';
+import { Link as RLink } from 'react-router-dom';
+import logo from '../assets/logo.png';
 
+const Footer = () => {
+  const bgColor = useColorModeValue('gray.100', 'gray.900');
 
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-
-export default function FooterComponent() {
   return (
-    <Flex p={4} as="footer"
-      justify="space-between" direction={{ base: "column", md: "row" }}
-      bg="brand.800" color="white"
-    >
-      <Flex align="center" direction={{ base: "column", md: "row" }}>
-        <Text m={2} color="gray.400">&copy; {new Date().getFullYear()} PollItUp - All Rights Reserved</Text>
-        <Stack m={2} spacing={3} direction="row" align="center">
-          {/*<RLink to="/contact">Contact us</RLink>
-          <RLink to="/reportissue">Report an issue</RLink>*/}
+    <Box as="footer" bg={bgColor} py={8} px={4}>
+      <Stack
+        spacing={{ base: 8, md: 0 }}
+        direction={{ base: 'column', md: 'row' }}
+        align={{ base: 'center', md: 'unset' }}
+        justifyContent={{ base: 'center', md: 'space-between' }} // Added justifyContent property
+      >
+        <Box maxW="100%" mr={20}>
+          <Image w="100px" src={logo} alt="PollitUp" />
+        </Box>
+        <VStack spacing={4} align="flex-start" flexGrow={1} >
+          <Text fontSize="md" fontWeight="bold">
+            Sitemap
+          </Text>
+          <VStack spacing={2} align="flex-start" color="gray.500">
+            <Link as={RLink} to="#">
+              Home
+            </Link>
+            <Link as={RLink} to="/discover">
+              Discover
+            </Link>
+            <Link as={RLink} to="/testimonials">
+              Testimonials
+            </Link>
+          </VStack>
+        </VStack>
+        <VStack spacing={4} align="flex-start" flexGrow={1} ml="auto">
+          <Text fontSize="md" fontWeight="bold">
+            Log In/Sign Up
+          </Text>
+          <VStack spacing={2} align="flex-start" color="gray.500">
+            <Link as={RLink} to="/login">
+              Log In
+            </Link>
+            <Link as={RLink} to="/resgister">
+              Register
+            </Link>
+          </VStack>
+        </VStack>
+        <Stack spacing={2} direction={{ base: 'column', md: 'row' }}>
+          <Button
+            leftIcon={<FaGithub />}
+            as={Link}
+            href="https://github.com/agamjotsingh18/pollitup.github.io"
+            target="_blank"
+            rounded="md"
+            colorScheme="purple"
+            color="white"
+            cursor="pointer"
+            _hover={{ textDecoration: 'none' }}
+          >
+            Follow on Github
+          </Button>
         </Stack>
-      </Flex>
-      <Stack m={2} spacing={4} direction="row" justify="center" align="center">
-
-        <ChakraLink href="https://github.com/agamjotsingh18/pollitup.github.io" target="_blank">
-          <FontAwesomeIcon icon={faGithub} />
-        </ChakraLink>
       </Stack>
-    </Flex>
-  )
-}
+
+      <Divider my={4} />
+
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        spacing={3}
+        justify="center" // Updated justify property to center align
+        align="center"
+      >
+        <Text fontSize="md" alignSelf="flex-end">
+          &copy; {new Date().getFullYear()} PollItUp - All Rights Reserved
+        </Text>
+        <Text fontSize="md" alignSelf="flex-end">
+          Made with ❤️ by Agamjot
+          <Link
+            href="https://github.com/MA-Ahmad"
+            textDecoration="underline"
+            _hover={{ textDecoration: 'none' }}
+          >
+          </Link>
+        </Text>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
