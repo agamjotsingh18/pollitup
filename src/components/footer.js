bf1a68d007355f162e92755760a738f33c700ebd
Soumyajit <mondalsoumya02@gmail.com>
2023-06-29 11:47:27 +0530

    Aligned the copyright text below the button

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,7 @@ import {
   Text,
   Button,
   useColorModeValue,
+  useColorMode,
 } from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiTwitter } from 'react-icons/fi';
@@ -16,7 +17,9 @@ import { Link as RLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Footer = () => {
+  const { colorMode } = useColorMode();
   const bgColor = useColorModeValue('gray.100', 'gray.900');
+  const buttonColorScheme = colorMode === 'light' ? 'purple' : 'blue';
 
   return (
     <Box as="footer" bg={bgColor} py={8} px={4}>
@@ -24,75 +27,77 @@ const Footer = () => {
         spacing={{ base: 8, md: 0 }}
         direction={{ base: 'column', md: 'row' }}
         align={{ base: 'center', md: 'unset' }}
-        justifyContent={{ base: 'center', md: 'space-between' }} // Added justifyContent property
+        justifyContent={{ base: 'center', md: 'space-between' }}
       >
         <Box maxW="100%" mr={20}>
           <Image w="100px" src={logo} alt="PollitUp" />
         </Box>
-        <VStack spacing={4} align="flex-start" flexGrow={1} >
-          <Text fontSize="md" fontWeight="bold">
+        <VStack spacing={4} align="flex-start" flexGrow={1}>
+          <Text fontSize="md" fontWeight="bold" color="gray.600">
             Sitemap
           </Text>
           <VStack spacing={2} align="flex-start" color="gray.500">
-            <Link as={RLink} to="#">
+            <Link as={RLink} to="#" color="blue.600">
               Home
             </Link>
-            <Link as={RLink} to="/discover">
+            <Link as={RLink} to="/discover" color="blue.600">
               Discover
             </Link>
-            <Link as={RLink} to="/testimonials">
+            <Link as={RLink} to="/testimonials" color="blue.600">
               Testimonials
             </Link>
           </VStack>
         </VStack>
         <VStack spacing={4} align="flex-start" flexGrow={1} ml="auto">
-          <Text fontSize="md" fontWeight="bold">
+          <Text fontSize="md" fontWeight="bold" color="gray.600">
             Log In/Sign Up
           </Text>
           <VStack spacing={2} align="flex-start" color="gray.500">
-            <Link as={RLink} to="/login">
+            <Link as={RLink} to="/login" color="blue.600">
               Log In
             </Link>
-            <Link as={RLink} to="/resgister">
+            <Link as={RLink} to="/register" color="blue.600">
               Register
             </Link>
           </VStack>
         </VStack>
-        <Stack spacing={2} direction={{ base: 'column', md: 'row' }}>
+        <VStack spacing={2} direction={{ base: 'column', md: 'row' }}>
           <Button
             leftIcon={<FaGithub />}
             as={Link}
             href="https://github.com/agamjotsingh18/pollitup.github.io"
             target="_blank"
             rounded="md"
-            colorScheme="purple"
-            color="white"
+            colorScheme={buttonColorScheme}
+            color={colorMode === 'light' ? 'white' : 'gray.800'}
             cursor="pointer"
             _hover={{ textDecoration: 'none' }}
           >
             Follow on Github
           </Button>
-        </Stack>
+        <Text fontSize="md" alignSelf="center" py={8}>
+          &copy; {new Date().getFullYear()} PollItUp - All Rights Reserved
+        </Text>
+        </VStack>
       </Stack>
 
-      <Divider my={4} />
+      <Divider my={6} borderColor="gray.300" />
 
       <Stack
         direction={{ base: 'column', md: 'row' }}
         spacing={3}
-        justify="center" // Updated justify property to center align
+        justify="center"
         align="center"
+        color="gray.600"
       >
-        <Text fontSize="md" alignSelf="flex-end">
-          &copy; {new Date().getFullYear()} PollItUp - All Rights Reserved
-        </Text>
-        <Text fontSize="md" alignSelf="flex-end">
-          Made with ❤️ by Agamjot
+        <Text fontSize="md" alignSelf="center">
+          Made with ❤️ by{' '}
           <Link
             href="https://github.com/MA-Ahmad"
             textDecoration="underline"
             _hover={{ textDecoration: 'none' }}
           >
+            Agamjot
           </Link>
         </Text>
       </Stack>
